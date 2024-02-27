37a395a4c90dfb69ea7872dd98c4b74c566d7618
Mustafa Bekirov <mustafos1999@gmail.com>
2024-02-27 10:57:09 +0100

    init capsule library

---
+++ CapsuleTabBar/CapsuleTabBar/CapsuleTabBar.swift
@@ -0,0 +1,14 @@
+//
+//  CapsuleTabBar.swift
+//  CapsuleTabBar
+//
+//  Created by Mustafa Bekirov on 26.02.2024.
+//
+
+public struct CapsuleTabBar {
+    public init() {}
+    
+    public func helloWorld() {
+        print("Hello, World!")
+    }
+}
