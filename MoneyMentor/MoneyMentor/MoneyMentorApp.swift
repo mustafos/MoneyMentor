569849e1276a75b0907959ce20087b16e73adb9d
mustafos <mustafos1999@icloud.com>
2024-09-17 11:44:13 +0200

    init: MoneyMentor project

---
+++ MoneyMentor/MoneyMentor/MoneyMentorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyMentorApp.swift
+//  MoneyMentor
+//
+//  Created by Mustafa Bekirov on 23.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoneyMentorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
