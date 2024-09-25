21ed79ccc8444418f2037d85a1d60dc836b6dc82
mustafos <mustafos1999@icloud.com>
2024-09-25 17:53:56 +0200

    feat: earninds layout with card

--- MoneyMentor/MoneyMentor/EarningsView.swift
+++ MoneyMentor/MoneyMentor/EarningsView.swift
@@ -16,11 +16,41 @@ struct EarningsView: View {
     @State var title = ""
     
     var body: some View {
-        ZStack {
-            Color.purple.ignoresSafeArea()
+        ZStack(alignment: .top) {
+            RoundedRectangle(cornerRadius: 20)
+                .fill(LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.3), Color.green.opacity(0.3)]), startPoint: .top, endPoint: .bottom))
+                .frame(maxWidth: .infinity, maxHeight: 325)
+                .ignoresSafeArea()
             VStack {
-                Text("Press The Button")
+                cardView
+                Text("$ 240.00")
                     .font(.largeTitle).bold().foregroundStyle(.white)
+                + Text(" pre click").font(.footnote).foregroundStyle(.white.opacity(0.8))
+                
+                HStack(spacing: 16) {
+                        VStack {
+                            Image(systemName: "play.rectangle")
+                            Text("Ad")
+                        }
+                        Capsule()
+                            .frame(width: 2, height: 50)
+                            .foregroundStyle(.secondary)
+                        HStack {
+                            Image(systemName: "dollarsign.arrow.trianglehead.counterclockwise.rotate.90")
+                            VStack(alignment: .leading) {
+                                Text("$ 1,500.00")
+                                    .font(.subheadline).bold()
+                                + Text(" pre click").font(.footnote).foregroundStyle(.black.opacity(0.8))
+                                Text("for 30 sec.")
+                                    .font(.footnote)
+                                    .foregroundStyle(.secondary)
+                            }
+                        }
+                    }
+                    .padding()
+                    .background(LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.6), Color.green.opacity(0.6)]), startPoint: .leading, endPoint: .trailing))
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                
                 Text("\(count)").font(.title).foregroundStyle(.white)
                 Spacer()
                 Button {
@@ -28,6 +58,10 @@ struct EarningsView: View {
                 } label: {
                     VStack {
                         Image(systemName: "hand.tap")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 60)
+                            .foregroundColor(.gray)
                         Text("\(buttonP)")
                         Text("Click in this area to earn money")
                     }
@@ -44,9 +78,9 @@ struct EarningsView: View {
                 Spacer()
             }.padding()
         }
-        .onReceive(time) { (_) in
-            minusNum()
-        }
+        //        .onReceive(time) { (_) in
+        //            minusNum()
+        //        }
     }
     
     func addPress() {
@@ -78,6 +112,59 @@ struct EarningsView: View {
         level += 10
         count = level
         showAlert = false
+    }
+}
+
+extension EarningsView {
+    var cardView: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Balance")
+                    .font(.headline)
+                    .padding(.bottom)
+                
+                Spacer()
+                // MastercardLogo
+                ZStack {
+                    Circle()
+                        .fill(Color.red)
+                        .frame(width: 46)
+                        .offset(x: -12)
+                        .blendMode(.multiply)
+                    
+                    Circle()
+                        .fill(Color.orange)
+                        .frame(width: 46)
+                        .offset(x: 12)
+                        .blendMode(.multiply)
+                }.compositingGroup()
+            }
+            
+            Spacer(minLength: 8)
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text("$17,370.52")
+                    .font(.system(.title, design: .monospaced, weight: .heavy))
+                Text("Expiry by 29 May, 2024")
+                    .font(.subheadline)
+                    .opacity(0.8)
+            }
+            .padding(.bottom)
+            HStack {
+                Text("**** **** **** 3022")
+                Spacer()
+                Text("05/24")
+            }
+            .font(.headline)
+                .fontDesign(.monospaced)
+        }
+        .font(.title3)
+        .foregroundColor(.white)
+        .frame(maxWidth: .infinity, maxHeight: 150)
+        .padding(20)
+        .background(.linearGradient(colors: [.teal, .blue], startPoint: .topLeading, endPoint: .bottomTrailing))
+        .clipShape(RoundedRectangle(cornerRadius: 20))
+        .shadow(color: Color.black.opacity(0.2), radius: 5, x: 5, y: 5)
     }
 }
 
